0d1c73cc95ac7ed5e9a3c65d6245f0b176c320f5
xiaolong1987 <xiaolongwu1987@sina.com>
2020-01-20 22:05:56 +0800

    ignore .idea

--- app/__init__.py
+++ app/__init__.py
@@ -44,9 +44,6 @@ def create_app(config_class=Config):
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
-    from app.default import bp as default_bp
-    app.register_blueprint(default_bp)
-
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
