62cf3dcc4564a276097a84ac83ee976f28ccc351
xiaolong1987 <xiaolongwu1987@sina.com>
2020-01-28 00:20:05 +0800

    installed flask-markdown==0.3 make post viewer a html file

--- app/visitor/routes.py
+++ app/visitor/routes.py
@@ -23,4 +23,17 @@ def index():
     # return redirect(url_for('default.explore'))
 
 
+@bp.route('/aa')
+#@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main.explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('./visitor.html', title=_('Explore'),
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
--- microblog.py
+++ microblog.py
@@ -1,9 +1,10 @@
 from app import create_app, db, cli
 from app.models import User, Post, Message, Notification, Task
+from flaskext.markdown import Markdown
 
 app = create_app()
 cli.register(app)
-
+Markdown(app)
 
 @app.shell_context_processor
 def make_shell_context():
