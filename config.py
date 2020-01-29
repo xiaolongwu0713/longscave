cba1de1cb41d53826d6ae30b300d524bfd783e54
xiaolong1987 <xiaolongwu1987@sina.com>
2020-01-29 11:10:43 +0800

    use mysql as database: longscave:xiaowu@longscave

--- config.py
+++ config.py
@@ -7,8 +7,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')db
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://longscave:xiaowu@localhost/longscave'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
