62c8b492bf37ea23ace9bba51daa4a87474c1745
xiaolong1987 <xiaolongwu1987@sina.com>
2020-01-30 10:22:56 +0800

    arrange visitor template in folder

--- app/visitor/routes.py
+++ app/visitor/routes.py
@@ -14,18 +14,9 @@ from app.visitor import bp
 # from longscave import app
 
 
-@bp.route('/hireme', methods=['GET', 'POST'])
-def index():
-    # flash('jump to main')
-    #flash(current_user.username)
-    return render_template('./hireMe.html')
-    # return '<title>我的第一个 HTML 页面</title>'
-    # return redirect(url_for('default.explore'))
-
-
 @bp.route('/')
 #@login_required
-def explore():
+def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
@@ -33,7 +24,28 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('./visitor.html', title=_('Explore'),
+    return render_template('/visitor/visitor.html', title=_('Explore'),
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
 
+@bp.route('/hireme', methods=['GET', 'POST'])
+def hireme():
+    # flash('jump to main')
+    #flash(current_user.username)
+    return render_template('/visitor/hireme.html')
+    # return '<title>我的第一个 HTML 页面</title>'
+    # return redirect(url_for('default.explore'))
+
+
+@bp.route('/aboutme', methods=['GET', 'POST'])
+def aboutme():
+    # flash('jump to main')
+    #flash(current_user.username)
+    return render_template('/visitor/aboutme.html')
+
+
+@bp.route('/course', methods=['GET', 'POST'])
+def course():
+    # flash('jump to main')
+    #flash(current_user.username)
+    return render_template('/visitor/course.html')
