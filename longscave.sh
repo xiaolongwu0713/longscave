0dfebabab45ae6992bff09d221b6069c641a387f
long <xiaolongwu0713@gmail.com>
2021-03-25 02:16:44 +0000

    pip install --upgrade pip

--- longscave.sh
+++ longscave.sh
@@ -240,6 +240,7 @@ chown -R xiaowu:xiaowu /home/xiaowu/longscave
 #mv microblog longscave
 cd longscave || exit
 source /usr/local/venv/longscave/bin/activate
+pip install --upgrade pip
 echo "pip install requirements"
 #pip install -r requirements.txt  -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 pip install -r requirements.txt
